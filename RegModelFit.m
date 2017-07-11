e76ee2e5dc065632d6eff6268c2e0d73e8fe5e03
tkushner <tkushner@engr2-5-125-dhcp.int.colorado.edu>
2017-07-11 14:39:26 -0600

    index res to go until zero (don't count zeros in min/max)

--- RegModelFit.m
+++ RegModelFit.m
@@ -1,6 +1,6 @@
 function [modelFits, stats] = RegModelFit(a0,lb,ub,gDelta,iDelta,patient,MAX)
 %% minimize least squares for 60min
-% a0=[2 1 -10]; 
+% a0=[2 1 -10];
 % lb = [0 0 -30];
 % ub=[2 2 0];
 % gDelta=12;
@@ -19,7 +19,7 @@ modelFits(MAX).RESstdev=[];
 for i=1:MAX;
     %only use the trial if there is no drop-out
     if mod(sum(patient(i).gtimes),5)==0
-   % if (abs(sum(patient(i).gtimes(2:end)-patient(i).gtimes(1:end-1))-5*length(patient(i).gtimes))<=5)
+        % if (abs(sum(patient(i).gtimes(2:end)-patient(i).gtimes(1:end-1))-5*length(patient(i).gtimes))<=5)
         n=1;
         tEND=length(patient(i).gCGM)-delta;
         modelFits(i).Fits=zeros(tEND,3);
@@ -31,12 +31,13 @@ for i=1:MAX;
             modelFits(i).RES(n)=sqrt(Res);
             n=n+1;
         end
-        
+        %Find first zero entry (to overcome fact that you cannot initialize empty matrix)
+        tENDfits=min(find(modelFits(i).Fits==[0,0,0]));
         %look at means & std dev for each parameter
-        modelFits(i).mean=mean(modelFits(i).Fits);
-        modelFits(i).stdev=std(modelFits(i).Fits);
-        modelFits(i).RESmean=mean(modelFits(i).RES);
-        modelFits(i).RESstdev=std(modelFits(i).RES);
+        modelFits(i).mean=mean(modelFits(i).Fits(1:tENDfits,1:3));
+        modelFits(i).stdev=std(modelFits(i).Fits(1:tENDfits,1:3));
+        modelFits(i).RESmean=mean(modelFits(i).RES(1:tENDfits));
+        modelFits(i).RESstdev=std(modelFits(i).RES(1:tENDfits));
     elseif sum(diff(patient(i).gtimes)~=5)==1
         n=1;
         tEND1=find(diff(patient(i).gtimes)~=5)-delta;
@@ -45,28 +46,28 @@ for i=1:MAX;
         modelFits(i).RES=zeros(tEND,1);
         if tEND1-1>delta
             for t=delta:tEND1-1
-            fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
-            [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
-            modelFits(i).Fits(n,:)=a;
-            modelFits(i).RES(n)=sqrt(Res);
-            n=n+1;
+                fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
+                [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
+                modelFits(i).Fits(n,:)=a;
+                modelFits(i).RES(n)=sqrt(Res);
+                n=n+1;
             end
         else
             for t=tEND1+delta:tEND2-1
-            fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
-            [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
-            modelFits(i).Fits(n,:)=a;
-            modelFits(i).RES(n)=sqrt(Res);
-            n=n+1;
+                fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
+                [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
+                modelFits(i).Fits(n,:)=a;
+                modelFits(i).RES(n)=sqrt(Res);
+                n=n+1;
             end
         end
         %Find first zero entry (to overcome fact that you cannot initialize empty matrix)
-        tENDfits=min(find(modelFits30min(i).Fits==[0,0,0]));
+        tENDfits=min(find(modelFits(i).Fits==[0,0,0]));
         %look at means & std dev for each parameter
         modelFits(i).mean=mean(modelFits(i).Fits(1:tENDfits,1:3));
         modelFits(i).stdev=std(modelFits(i).Fits(1:tENDfits,1:3));
-        modelFits(i).RESmean=mean(modelFits(i).RES);
-        modelFits(i).RESstdev=std(modelFits(i).RES);
+        modelFits(i).RESmean=mean(modelFits(i).RES(1:tENDfits));
+        modelFits(i).RESstdev=std(modelFits(i).RES(1:tENDfits));
     else continue
     end
     
@@ -77,7 +78,7 @@ stats.mean=nanmean(MEAN);
 stats.stdev=nanstd(MEAN);
 stats.RESmean=nanmean(RESMEAN);
 stats.RESstdev=nanstd(RESMEAN);
-stats.RESmax=max(max(padcat(modelFits(1:end).RES)));
-stats.RESmin=min(min(padcat(modelFits(1:end).RES)));
+stats.RESmax=max(max(padcat(modelFits(1:end).RES(1:tENDfits))));
+stats.RESmin=min(min(padcat(modelFits(1:end).RES(1:tENDfits))));
 stats.RES95=1.96*stats.RESstdev;
 end
