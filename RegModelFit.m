e3244e76ef10cf7d7daff65b306c704d936f93ba
tkushner <tkushner@engr2-5-125-dhcp.int.colorado.edu>
2017-07-12 15:57:39 -0600

    -added plots (all time steps, averaged and by individual trial)

--- RegModelFit.m
+++ RegModelFit.m
@@ -1,13 +1,6 @@
 function [modelFits, stats] = RegModelFit(a0,lb,ub,gDelta,iDelta,patient,MAX)
-%% minimize least squares for 60min
-% a0=[2 1 -10];
-% lb = [0 0 -30];
-% ub=[2 2 0];
-% gDelta=12;
-% iDelta=12;
 delta=max(gDelta,iDelta)+1;
 
-basename=strcat('g',gDelta,'i',iDelta);
 %initialize struct for modelFit
 modelFits(MAX).Fits=[];
 modelFits(MAX).RES=[];
@@ -17,20 +10,20 @@ modelFits(MAX).RESmean=[];
 modelFits(MAX).RESstdev=[];
 modelFits(MAX).RESmin=[];
 
-for i=1:MAX;
+for i = 1:MAX
         tEND=length(patient(i).gCGM)-delta;
         modelFits(i).Fits=zeros(tEND,3);
         modelFits(i).RES=zeros(tEND,1);
 end
 
-parfor i=1:MAX;
+parfor i = 1:MAX
     
     tEND=length(patient(i).gCGM)-delta;
     %check for trials with no errors (drop out/ extra points / etc)
     if min(diff(patient(i).gtimes))==5 && max(diff(patient(i).gtimes)==5)
         n=1;
         for t=delta:tEND-1
-            fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
+            fun=@(a)(patient(i).gCGM(t+gDelta)-a(1)*patient(i).gCGM(t-gDelta)-a(2)*patient(i).gCGM(t)-a(3)*patient(i).gIOB(t-iDelta))^2;
             [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
             modelFits(i).Fits(n,:)=a;
             modelFits(i).RES(n)=sqrt(Res);
@@ -52,23 +45,22 @@ parfor i=1:MAX;
         tEND2=length(patient(i).gCGM)-delta;
         if (tEND1-1)>delta
             for t=delta:(tEND1-1)
-                fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
+            fun=@(a)(patient(i).gCGM(t+gDelta)-a(1)*patient(i).gCGM(t-gDelta)-a(2)*patient(i).gCGM(t)-a(3)*patient(i).gIOB(t-iDelta))^2;
                 [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
                 modelFits(i).Fits(n,:)=a;
                 modelFits(i).RES(n)=sqrt(Res);
                 n=n+1;
             end
             for t=(SWITCH+delta+1):(tEND2-1)
-                fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
+            fun=@(a)(patient(i).gCGM(t+gDelta)-a(1)*patient(i).gCGM(t-gDelta)-a(2)*patient(i).gCGM(t)-a(3)*patient(i).gIOB(t-iDelta))^2;
                 [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
                 modelFits(i).Fits(n,:)=a;
                 modelFits(i).RES(n)=sqrt(Res);
                 n=n+1;
             end
         else
-            n=SWITCH;
-            for t=SWITCH+delta+1:tEND2-1
-                fun=@(a)(a(1)*patient(i).gCGM(t-gDelta)+a(2)*patient(i).gCGM(t)+a(3)*patient(i).gIOB(t-iDelta)-patient(i).gCGM(t+gDelta))^2;
+            for t = (SWITCH+delta+1):(tEND2-1)
+            fun=@(a)(patient(i).gCGM(t+gDelta)-a(1)*patient(i).gCGM(t-gDelta)-a(2)*patient(i).gCGM(t)-a(3)*patient(i).gIOB(t-iDelta))^2;
                 [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
                 modelFits(i).Fits(n,:)=a;
                 modelFits(i).RES(n)=sqrt(Res);
--- main.m
+++ main.m
@@ -50,6 +50,7 @@ iDelta=6;
 tic
 [modelFits30min, stats30min]=RegModelFit(a0,lb,ub,gDelta,iDelta,patient,MAX);
 toc
+
 %% minimize least squares for 45min
 a0=[2 1 -10]; 
 lb = [0 0 -30];
@@ -96,3 +97,161 @@ iDelta=9;
 tic
 [modelFitsG60I45min, statsG60I45min]=RegModelFit(a0,lb,ub,gDelta,iDelta,patient,MAX);
 toc
+
+%% plot fit models 30min
+
+ID=1:20;
+gDelta=6;
+iDelta=6;
+delta=max(gDelta,iDelta)+1;
+
+
+for i=ID
+    tEND=length(patient(i).gtimes);
+    modelFits30min(i).predictAVG=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta+1
+        modelFits30min(i).predictAVG(m)=patient(i).gCGM(m);
+    end
+    for m = delta:(tEND-gDelta)
+        modelFits30min(i).predictAVG(m+gDelta)=modelFits30min(i).mean(1)*patient(i).gCGM(m)+modelFits30min(i).mean(2)*patient(i).gCGM(m-gDelta)+modelFits30min(i).mean(3)*patient(i).gIOB(m-iDelta);
+    end
+end
+
+gDelta=12;
+iDelta=12;
+delta=max(gDelta,iDelta)+1;
+for i=ID
+        tEND=length(patient(i).gtimes);
+    modelFits60min(i).predict=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta
+        modelFits60min(i).predict(m)=patient(i).gCGM(m);
+    end
+    for m=delta:tEND-gDelta
+        modelFits60min(i).predict(m+gDelta)=modelFits60min(i).mean(1)*patient(i).gCGM(m-gDelta)+modelFits60min(i).mean(2)*patient(i).gCGM(m)+modelFits60min(i).mean(3)*patient(i).gIOB(m-iDelta);
+    end
+end
+
+gDelta=9;
+iDelta=9;
+delta=max(gDelta,iDelta)+1;
+for i=ID
+        tEND=length(patient(i).gtimes);
+        modelFits45min(i).predict=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta
+        modelFits45min(i).predict(m)=patient(i).gCGM(m);
+    end
+    for m=delta:tEND-gDelta
+        modelFits45min(i).predict(m+gDelta)=modelFits45min(i).mean(1)*patient(i).gCGM(m-gDelta)+modelFits45min(i).mean(2)*patient(i).gCGM(m)+modelFits45min(i).mean(3)*patient(i).gIOB(m-iDelta);
+    end
+end
+
+for i=ID
+    % plot
+    figure(i)
+    subplot(2,1,1)
+    plot(patient(i).gtimes,patient(i).gCGM,'-o',patient(i).gtimes,modelFits30min(i).predictAVG,patient(i).gtimes,modelFits60min(i).predict,patient(i).gtimes,modelFits45min(i).predict)
+    %plot(patient(i).gtimes,patient(i).gCGM,'-o',patient(i).gtimes,modelFits30min(i).predict,patient(i).gtimes,modelFits30min(i).predictAVG)
+    ylabel('CGM')
+    xlabel('minutes since start')
+    title(strcat(patient(i).datetime(1)))
+    subplot(2,1,2)
+    plot(patient(i).times,patient(i).IOB)
+    ylabel('IOB')
+    xlabel('minutes since start')
+    title(strcat(patient(i).datetime(end)))
+end
+
+%%
+
+ID=1:20;
+gDelta=6;
+iDelta=6;
+delta=max(gDelta,iDelta)+1;
+
+for i=ID
+    tEND=length(patient(i).gtimes);
+    modelFits30min(i).predict=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta+1
+        modelFits30min(i).predict(m)=patient(i).gCGM(m);
+    end
+    n=1;
+    for m = (delta+gDelta):tEND-3
+        modelFits30min(i).predict(m)=modelFits30min(i).Fits(n,1)*patient(i).gCGM(n)+modelFits30min(i).Fits(n,2)*patient(i).gCGM(n+gDelta)+modelFits30min(i).Fits(n,3)*patient(i).gIOB(n+iDelta);
+        n=n+1;
+    end
+    for m= tEND-3:tEND
+        modelFits30min(i).predict(m)=patient(i).gCGM(m);
+    end
+end
+
+gDelta=9;
+iDelta=9;
+delta=max(gDelta,iDelta)+1;
+
+
+for i=ID
+    tEND=length(patient(i).gtimes);
+    modelFits45min(i).predict=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta+1
+        modelFits45min(i).predict(m)=patient(i).gCGM(m);
+    end
+    for m = delta:tEND-3
+        modelFits45min(i).predict(m+gDelta)=modelFits45min(i).Fits(m-gDelta,1)*patient(i).gCGM(m-gDelta)+modelFits45min(i).Fits(m-gDelta,2)*patient(i).gCGM(m)+modelFits45min(i).Fits(m-gDelta,3)*patient(i).gIOB(m-iDelta);
+    end
+    for m= tEND-3:tEND
+        modelFits45min(i).predict(m)=patient(i).gCGM(m);
+    end
+end
+
+gDelta=12;
+iDelta=12;
+delta=max(gDelta,iDelta)+1;
+
+
+for i=ID
+    tEND=length(patient(i).gtimes);
+    modelFits60min(i).predict=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta+1
+        modelFits60min(i).predict(m)=patient(i).gCGM(m);
+    end
+    for m = delta:tEND-3
+        modelFits60min(i).predict(m+gDelta)=modelFits60min(i).Fits(m-gDelta,1)*patient(i).gCGM(m-gDelta)+modelFits60min(i).Fits(m-gDelta,2)*patient(i).gCGM(m)+modelFits60min(i).Fits(m-gDelta,3)*patient(i).gIOB(m-iDelta);
+    end
+    for m= tEND-3:tEND
+        modelFits60min(i).predict(m)=patient(i).gCGM(m);
+    end
+end
+
+gDelta=24;
+iDelta=24;
+delta=max(gDelta,iDelta)+1;
+
+for i=ID
+    tEND=length(patient(i).gtimes);
+    modelFits120min(i).predict=zeros(length(patient(i).gCGM),1);
+    for m=1:delta+gDelta+1
+        modelFits120min(i).predict(m)=patient(i).gCGM(m);
+    end
+    for m = delta:tEND-3
+        modelFits120min(i).predict(m+gDelta)=modelFits120min(i).Fits(m-gDelta,1)*patient(i).gCGM(m-gDelta)+modelFits120min(i).Fits(m-gDelta,2)*patient(i).gCGM(m)+modelFits120min(i).Fits(m-gDelta,3)*patient(i).gIOB(m-iDelta);
+    end
+    for m= tEND-3:tEND
+        modelFits120min(i).predict(m)=patient(i).gCGM(m);
+    end
+end
+
+for i=ID
+    % plot
+    figure(i)
+    subplot(2,1,1)
+    plot(patient(i).gtimes,patient(i).gCGM,'-o',patient(i).gtimes,modelFits30min(i).predict,patient(i).gtimes,modelFits45min(i).predict,patient(i).gtimes,modelFits60min(i).predict,patient(i).gtimes,modelFits120min(i).predict)
+    legend('CGM data','30min','45min','60min','120min')
+    title('CGM actual vs Predicted values for various time steps')
+    ylabel('Glucose Measurements (mg/dL)')
+    xlabel('minutes since start of trial')
+    subplot(2,1,2)
+    plot(patient(i).times,patient(i).IOB)
+    ylabel('Insulin On Board')
+    xlabel('minutes since start of trial')
+    title(allfiles(i).name)
+end
