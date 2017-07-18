1355090771f861b41c8de3335ed74fdf4cd0fe15
tkushner <tkushner@engr2-4-121-dhcp.int.colorado.edu>
2017-07-18 15:55:11 -0600

    windowregmodelfit is working!

--- WindowRegModelFit.m
+++ WindowRegModelFit.m
@@ -1,12 +1,12 @@
-%optimize across windows within each trial
-%function [modelFits, stats] = WindowRegModelFit(a0,lb,ub,gDelta,iDelta,patient,MAX)
+%optimize across windows within each trial using nls instead of fmincon
+function [modelFits, stats] = WindowRegModelFit(a0,lb,ub,gDelta,iDelta,patient,MAX)
 
 a0=[.5 .5 -2.5];
 lb = [0 0 -10];
-ub=[2 2 0];
+ub=[2 2 -1];
 gDelta=6;
 iDelta=6;
-MAX=10;
+MAX=30;
 
 delta=max(gDelta,iDelta)+1;
 
@@ -23,8 +23,8 @@ modelFits(MAX).STD=[];
 
 %define windows for parsing & overlap (default: 300min (60 steps), 25min
 %overlap (5)
-stepsz= 15;
-ovlp = 2;
+stepsz= 20;
+ovlp = 5;
 step=stepsz-ovlp;
 
 for i = 1:MAX
@@ -51,41 +51,31 @@ for i = 1:MAX
                     CGM(:,j+1)=patient(i).gCGM((step*j):(step*j+stepsz-1));
                     IOB(:,j+1)=patient(i).gIOB((step*j):(step*j+stepsz-1));
                 end
-            else 
+            else
                 continue
             end
-            %construct matrix
             
             for j=1:Nw
-            B=[CGM(delta+gDelta:end,j);1];
-            A=[CGM(1:end-2*gDelta,j), CGM(gDelta+1:end-gDelta,j), IOB(1:end-2*iDelta,j);[1,1,0]];
-            options = optimoptions('lsqlin','Algorithm','interior-point','Display','iter');
-            [x, resnorm, residual, exitflag, output, lambda]=lsqlin(A,B,[],[],[],[],lb,ub);
-            modelFits(i).Fits(j,:)=x;
-            modelFits(i).RES=residual;
-            modelFits(i).ResMin=min(modelFits(i).RES);
-            modelFits(i).ResMax=max(modelFits(i).RES);
-            modelFits(i).ResMean=nanmean(modelFits(i).RES);
-            modelFits(i).ResStd=nanstd(modelFits(i).RES);
-            
-            if Nw>1
-                modelFits(i).MEAN=nanmean(modelFits(i).Fits);
-                modelFits(i).STD=nanstd(modelFits(i).Fits);
-            else
-                modelFits(i).MEAN=nan(1,3);
-                modelFits(i).STD=NaN;
+                %construct matrix
+                B=[CGM(delta+gDelta:end,j);1];
+                A=[CGM(1:end-2*gDelta,j), CGM(gDelta+1:end-gDelta,j), IOB(1:end-2*iDelta,j);[1,1,0]];
+                options = optimoptions('lsqlin','Algorithm','interior-point');
+                [x, resnorm, residual, exitflag, output, lambda]=lsqlin(A,B,[],[],[],[],lb,ub,a0,options);
+                modelFits(i).Fits(j,:)=x;
+                modelFits(i).RES=residual;
+                modelFits(i).ResMin=min(modelFits(i).RES);
+                modelFits(i).ResMax=max(modelFits(i).RES);
+                modelFits(i).ResMean=nanmean(modelFits(i).RES);
+                modelFits(i).ResStd=nanstd(modelFits(i).RES);
+                
+                if Nw>1
+                    modelFits(i).MEAN=nanmean(modelFits(i).Fits);
+                    modelFits(i).STD=nanstd(modelFits(i).Fits);
+                else
+                    modelFits(i).MEAN=nan(1,3);
+                    modelFits(i).STD=NaN;
+                end
             end
-            end
-%             for j=1:Nw
-%                 fun=@(a)(sum((CGM(delta+gDelta:end,j)-(a(1)*CGM((delta-gDelta):(end-2*gDelta),j)+ ...
-%                     a(2)*CGM(delta:(end-gDelta),j)+a(3)*IOB((delta-iDelta):(end-2*iDelta),j))).^2));
-%                 [a,Res]=fmincon(fun,a0,[-1,-1,0],[-1],[],[],lb,ub);
-%                 modelFits(i).Fits(j,:)=a;
-%                 modelFits(i).RES(j,:)=sqrt(Res)/stepsz;
-%             end
-            
-            
-        
         else
             continue
         end
@@ -94,26 +84,23 @@ for i = 1:MAX
         continue
     end
 end
-%
-%     %check for trials with no errors (drop out/ extra points / etc)
-%     if min(diff(patient(i).gtimes))==5 && max(diff(patient(i).gtimes)==5)
-%         fun=@(a)(sum((patient(i).gCGM((delta+gDelta):end)-(a(1)*patient(i).gCGM((delta-gDelta):(end-2*gDelta))+ ...
-%             a(2)*patient(i).gCGM(delta:(end-gDelta))+a(3)*patient(i).gIOB((delta-iDelta):(end-2*iDelta)))).^2));
-%         [a,Res]=fmincon(fun,a0,[],[],[],[],lb,ub);
-%         modelFits(i).Fits=a;
-%         modelFits(i).RES=sqrt(Res);
-%     else
-%         continue
-%     end
-%     %look at means & std dev for each parameter
-% end
-% [MEAN]=padcat(modelFits(1:end).Fits);
-% [RESES]=padcat(modelFits(1:end).RES);
-% stats.mean=nanmean(MEAN);
-% stats.stdev=nanstd(MEAN);
-% stats.RESmean=nanmean(RESES);
-% stats.RESstdev=nanstd(RESES);
-% stats.RESmax=max(RESES);
-% stats.RESmin=min(RESES);
-% stats.RES95=1.96*stats.RESstdev;
-%end
+for i=1:length(modelFits)
+    %check if empty
+    if isempty(modelFits(i).Fits)
+        modelFits(i).Fits=nan(1,3);
+        modelFits(i).RES=nan(1);
+    end
+end
+%calculate stats
+[MEAN]=vertcat(modelFits(1:end).Fits);
+[RESES]=vertcat(modelFits(1:end).RES);
+stats.mean=nanmean(MEAN);
+stats.stdev=nanstd(MEAN);
+stats.RESmean=nanmean(RESES);
+stats.RESstdev=nanstd(RESES);
+stats.RESmax=max(RESES);
+stats.RESmin=min(RESES);
+stats.RES95=1.96*stats.RESstdev;
+end
+
+
